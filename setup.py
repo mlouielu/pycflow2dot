41fc40f2a061218d1dd98f0b80be0f5176143b04
Ioannis Filippidis <jfilippidis@gmail.com>
2017-10-24 06:25:15 -0700

    REL: add Trove classifiers for Python 3.4, 3.5, 3.6
    explicit is better than implicit [PEP 20].

--- setup.py
+++ setup.py
@@ -38,6 +38,9 @@ def run_setup():
             'Programming Language :: Python',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
             'Topic :: Software Development'],
         keywords=['c', 'call graph', 'control flow', 'dot',
                   'latex', 'cflow'])
